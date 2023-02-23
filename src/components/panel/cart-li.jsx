33a4e70166f254fdee15ab010465337069984754
Gjeev <g_jeevika@me.iitr.ac.in>
2023-02-23 22:12:03 +0530

    small changes before deploy

--- src/components/panel/cart-li.jsx
+++ src/components/panel/cart-li.jsx
@@ -2,7 +2,7 @@ import Tooltip from "@mui/material/Tooltip";
 
 export default function List1() {
     return (
-        <li className="list">
+        <li className="list cart-list">
           <Tooltip placement="top" title="upload to cart">
             <img src="/images/panelicon2.png" />
           </Tooltip>
